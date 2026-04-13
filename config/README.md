d8f7b226edecab50473e9ac0eddbd04617982b77
GreenJon902 <greenjon902@gmail.com>
2026-04-13 15:14:43 +0100

    Add config file syncing and some of the config files

---
+++ config/README.md
@@ -0,0 +1,20 @@
+# Config
+This module manages the contents and deployments of the configuration files.
+
+## Deployment
+The `sync.py` script will copy the of the contents of folders specified in `conf-map.ini` to their respective destinations (also specified by that file).
+This script will check for any discrepancies between the destination folders and the local folders, and ask you what to do in each case. This will not make any changes without user-input.
+
+The `conf-map.ini` should contain a section with header `conf-map`, which should contain key-value pairs of `<local-folder-name>=<destination-folder-path>`.
+
+## Testing
+Running `sync.py test-map.ini` will map the folders to `./test/...`. You may need to create the destination folders beforehand. Then mess around whith files in the test folder to see that everything is working.
+
+You can also use the `-d`/`--dry-run` flag to test the program without making any changes.
+
+## Extra information
+Any files we copy have the following header pre-pended to them:
+```
+# This is a G-DemMAIN synced config file, and may be overwritten when sync is run. Please do not modify this line, and leave it as the first line of this file.
+```
+This line is used to check if a file is created by the `sync.py` script, and it is expected this is left as it is, on the first line.
---
+++ config/sync.py
@@ -0,0 +1,160 @@
+from configparser import ConfigParser
+from argparse import ArgumentParser
+import os
+import sys
+
+
+# Constants
+HEADER_LINE = "# This is a G-DemMAIN synced config file, and may be overwritten when sync is run. Please do not modify this line, and leave it as the first line of this file."
+DIFF_IND_LENGTH = 50
+
+RESET = "\033[0m" if sys.stdout.isatty() else ""     # If we are in a terminal then colors probably work
+PATH_COL = "\033[94m" if sys.stdout.isatty() else ""
+
+# Parse arguments
+parser = ArgumentParser(description="See README.md")
+parser.add_argument("confmap",
+                    nargs   = "?",  # Declare this argument as optional
+                    default = "conf-map.ini",
+                    help    = "The path of the config map to use, defaults to ./conf-map.ini")  # The name we refer
+parser.add_argument("-d", "--dry-run",
+                    action  = "store_true",  # This sets default value to false
+                    help    = "Is this a dry run (e.g. if this flag is set then no changes will be made)")
+args = parser.parse_args()
+
+# Log run-info
+print("Executing in", os.getcwd())
+print("Ran with args", sys.argv, "which parsed to", args)
+
+# Load conf-map
+config = ConfigParser()
+if not os.path.exists(args.confmap):
+    raise Exception("Config map file does not exist - " + args.confmap)
+config.read(args.confmap)
+conf_map = dict(config["conf-map"])
+print("Loaded map:", conf_map)
+
+# Check source and destination folders exist
+for (source, destination) in conf_map.items():
+    if not os.path.exists(source):
+        raise Exception("Source folder does not exist - " + source)
+    if not os.path.exists(destination):
+        raise Exception("Destination folder does not exist - " + destination)
+    
+# Check for any files that we have previously added to the destination folder that should no longer be there
+for (source, destination) in conf_map.items():
+    for (root, dirs, files) in os.walk(destination):
+        for file in files:
+            file = os.path.join(root, file)
+            
+            # Check if file exists in both source and destination
+            relpath = os.path.relpath(file, destination)
+            expected_source_path = os.path.join(source, relpath)
+            if not os.path.exists(expected_source_path):
+                
+                # Check if this script created the file (it has the header)
+                with open(file, "r") as f:
+                    header = f.readline(len(HEADER_LINE))
+                    if header == HEADER_LINE:
+                        
+                        # Handle this file
+                        while True:
+                            action = input(f"{PATH_COL}{file}{RESET} has header, but was not found at {PATH_COL}{expected_source_path}{RESET}. Should this file be deleted[d] or ignored[i]? ")
+                            if action == "d":
+                                print(f"Removing {PATH_COL}{file}{RESET}")
+                                if not args.dry_run:
+                                    os.remove(file)
+                                break
+                            elif action == "i":
+                                print(f"Ignoring {PATH_COL}{file}{RESET}")
+                                break
+                            else:
+                                print("Invalid option, expected d or i")
+                                continue
+
+# Check for any new or changed files
+for (source_folder, destination_folder) in conf_map.items():
+    for (root, dirs, files) in os.walk(source_folder):
+        for source_file in files:
+            
+            # Paths
+            source_file = os.path.join(root, source_file)
+            relpath = os.path.relpath(source_file, source_folder)
+            destination_file = os.path.join(destination_folder, relpath)
+            
+            # Find (if we need to) what to ask the user
+            if not os.path.exists(destination_file):  # If the destination does not exist then ask the user if we want to copy the file
+                prompt = f"{PATH_COL}{source_file}{RESET} not found at {PATH_COL}{destination_file}{RESET}, should it be coppied[c] or ignored[i]? "
+            else:
+                # The destination_file exists
+            
+                
+                destination_fo = open(destination_file, "r")
+                if (found_header_line := destination_fo.readline(len(HEADER_LINE))) != HEADER_LINE:  # Check if we created the destination file
+                    raise Exception(f"A source file maps to a destination file that we did not create - source: {source_file}, destination: {destination_file}. The first line (partial): {found_header_line}")
+                else:
+                    # We created the destination file, so compare if their contents are the same
+                    
+                    # Load files
+                    source = open(source_file, "r").read()
+                    destination_fo.readline()
+                    destination = destination_fo.read()
+                    
+                    if source != destination:
+                        # Don't match so show diff and ask user if we want to replace it
+                        print("Diff (without header):")
+                        
+                        # Print files next to eachother with line numbers
+                        source = source.split("\n")
+                        destination = destination.split("\n")
+                        line_count = max(len(source), len(destination))
+                        for i in range(line_count):
+                            # Pad lines so they are the same length
+                            source_line = source[i] if i < len(source) else ""
+                            destination_line = destination[i] if i < len(destination) else ""
+                            source_line = source_line.ljust(max_line_len := max(len(source_line), len(destination_line), DIFF_IND_LENGTH))
+                            destination_line = destination_line.ljust(max_line_len)
+                            # Print lines, while splitting them if they're too long
+                            first_line = True
+                            while source_line != "":
+                                end = min(DIFF_IND_LENGTH, len(source_line))
+                                line_numb = str(i+1) + "." if first_line else ""  # Only write line_numb on the start of the line
+                                print(line_numb.ljust(len(str(line_count)) + 1),  # Pad line_number so all lines are aligned
+                                      "  ",
+                                      source_line[0:end].ljust(DIFF_IND_LENGTH), 
+                                      "  ",
+                                      destination_line[0:end].ljust(DIFF_IND_LENGTH))
+                                source_line = source_line[end:]
+                                destination_line = destination_line[end:]
+                                first_line = False
+                            
+                        
+                        
+
+                        prompt = f"{PATH_COL}{source_file}{RESET} does not match {PATH_COL}{destination_file}{RESET}, should it be replaced[r] or ignored[i]? "
+                    else: 
+                        print(f"{PATH_COL}{source_file}{RESET} matches {PATH_COL}{destination_file}{RESET}")
+                        continue  # Source and destination match so go to next file
+
+                
+            # Handle the users action
+            while True:
+                action = input(prompt)
+                if action == "c" or action == "r":
+                    print(f"Copying {PATH_COL}{source_file}{RESET} (with header) to {PATH_COL}{destination_file}{RESET}")
+                    if not args.dry_run:
+                        # Create folder if required
+                        if not os.path.exists(parent := os.path.dirname(destination_file)):
+                            print(f"Making parent folder {PATH_COL}{parent}{RESET}")
+                            os.makedirs(parent)
+                        # Add header to source file, then write to destination_file
+                        open(destination_file, "w").write(HEADER_LINE + "\n" + open(source_file, "r").read())
+                    break
+                elif action == "i":
+                    print(f"Ignoring {PATH_COL}{source_file}{RESET}")
+                    break
+                else:
+                    print("Invalid option, expected d or i")
+                    continue
+
+print("Sync complete!")
