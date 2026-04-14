337c2983db7be7bc93af99ab4f29895334388331
GreenJon902 <greenjon902@gmail.com>
2026-04-14 10:20:25 +0100

    Made g_mc/stop_service.sh work

--- g_mc/stop_service.sh
+++ g_mc/stop_service.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+# Extract RCON settings from server.properties
+rcon_enabled=$(grep "^enable-rcon=" /var/lib/g_mc/minecraft/server.properties | cut -d"=" -f2-)
+rcon_port=$(grep "^rcon.port=" /var/lib/g_mc/minecraft/server.properties | cut -d"=" -f2-)
+rcon_password=$(grep "^rcon.password=" /var/lib/g_mc/minecraft/server.properties | cut -d"=" -f2-)
+
 # Check if RCON is enabled
-rcon_enabled = $(grep "^enable-rcon=" server.properties | cut -d"=" -f2-)
 if [[ "$rcon_enabled" != "true" ]]; then
     echo "Warning, RCON is disabled, cannot stop the Minecraft server!"
     exit 1;
+elif [[ -z "$rcon_password" ]]; then  # Check if password length is equal to 0 (so was not set)
+    echo "Warning, RCON is disabled (as no password was set), cannot stop the Minecraft server!"
+    exit 1;
 fi
-
-# Get RCON port and password
-rcon_port = $(grep "^rcon-port=" server.properties | cut -d"=" -f2-)
-rcon_password = $(grep "^rcon-password=" server.properties | cut -d"=" -f2-)
 
 # Stop the server
 echo "Stopping server..."
-/opt/infra/g_mc/mcrcon/mcrcon -P $rcon_port -p $rcon_password -w 5 "say Server is stopping!" stop  # Accounce server is restarting, wait five seconds, then stop the server
+/opt/infra/g_mc/mcrcon/mcrcon -P $rcon_port -p $rcon_password -w 5 "say Server is stopping!" stop  # Announce server is restarting, wait five seconds, then stop the server
