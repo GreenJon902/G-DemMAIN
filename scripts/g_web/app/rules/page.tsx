742eca1b60727ab003eaa967002d1399affb6ae4
GreenJon902 <greenjon902@gmail.com>
2026-04-17 13:03:43 +0100

    Setting up of more of g_web or somin god knows what I'm doing

--- scripts/g_web/app/rules/page.tsx
+++ scripts/g_web/app/rules/page.tsx
@@ -1,7 +1,5 @@
-import Link from 'next/link'
-
 export default function Page() {
-  return (
-    <p>Todo</p>
-  );
+    return (
+        <p>Todo</p>
+    );
 }
---
+++ scripts/g_web/eslint.config.mjs
@@ -0,0 +1,32 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import tailwind from "eslint-plugin-tailwindcss";
+
+const eslintConfig = defineConfig([
+    
+    // Next:
+    ...nextVitals,
+    ...nextTs,
+    globalIgnores([
+        // Default ignores of eslint-config-next:
+        ".next/**",
+        "out/**",
+        "build/**",
+        "next-env.d.ts"
+    ]),
+
+    // Tailwind
+    tailwind.configs["flat/recommended"],
+
+    // Styling rules
+    {
+        rules: {
+            semi: ["error", "always"],
+            indent: ["warn", 4],
+            "comma-dangle": ["warn", "never"]
+        }
+    }
+]);
+
+export default eslintConfig;
