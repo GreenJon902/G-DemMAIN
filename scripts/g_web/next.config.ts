a66501ee42359cfdc57d7ebc05bb76c25c5547a1
GreenJon902 <greenjon902@gmail.com>
2026-04-19 12:48:23 +0100

    Fixed cross origins accross local network

--- scripts/g_web/next.config.ts
+++ scripts/g_web/next.config.ts
@@ -1,11 +1,20 @@
 import type { NextConfig } from "next";
 
+// Find local ip address for use in dev-origins
+var os = require('os');
+var networkInterfaces = os.networkInterfaces();
+const allowedDevOrigins = [];
+Object.values(networkInterfaces).forEach(v => v.forEach(w => allowedDevOrigins.push(w.address)));
+console.log("Allowing dev origins", allowedDevOrigins);
+
+
 const nextConfig: NextConfig = {
     /* config options here */
     reactCompiler: true,
     images: {
         remotePatterns: [new URL("https://api.mcheads.org/head/**/256/hat")]
-    }
+    },
+    allowedDevOrigins: allowedDevOrigins
 };
 
 export default nextConfig;
