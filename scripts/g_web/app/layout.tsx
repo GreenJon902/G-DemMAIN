02ad72047626b6f55cc7ce88e49c3836d0fc738b
GreenJon902 <greenjon902@gmail.com>
2026-04-20 14:00:08 +0100

    Fixed commit save attempt to make not-found background fullscreen on mobile

--- scripts/g_web/app/layout.tsx
+++ scripts/g_web/app/layout.tsx
@@ -1,13 +1,30 @@
-import "./globals.css";
+"use client";
+
+import "@/app/globals.css";
+import { useEffect, useState } from "react";
+import { getNotFoundBackground } from "./not-found";
 
 export default function Layout({
     children
 }: {
     children: React.ReactNode,
 }) {
+    // If this is the not-found page, then we want to render the missing texture pattern
+    // This needs to be done here so it goes above the notch on an iPhone
+    // We check if we're on the not found page by looking for any ellement with [data-is-not-found] set
+    const [extraClass, setExtraClass] = useState("");   
+    const [extraStyle, setExtraStyle] = useState({});   
+    useEffect(() => {
+        // If we find [data-is-not-found] then replace the background with the not-found background
+        if (document.querySelector("[data-is-not-found]")) {
+            const bgStyle = getNotFoundBackground();
+            setExtraStyle(bgStyle);
+        }
+    }, []);
+
     return (
         <html>
-            <body>
+            <body className={`bg-gray-900 p-4 text-white ${extraClass}`} style={extraStyle}>
                 {children}
             </body>
         </html>
--- scripts/g_web/app/not-found.tsx
+++ scripts/g_web/app/not-found.tsx
@@ -1,10 +1,26 @@
+import { CSSProperties } from "react";
+
+/*
+ * Returns the classes and style array for setting the background for this page.
+ * This allows layout.tsx to apply it directly to the body, so it fills the entire viewport.
+ */
+export function getNotFoundBackground(): CSSProperties {
+    return {
+        // Draw missing-missing texture pattern in bg
+        // We use --bg-opacity to fade the background in as it is applied in post. This is set by the bg-opacity-fade-in class
+        backgroundImage: 
+            "repeating-conic-gradient(" + 
+            "rgba(0, 0, 0, var(--bg-opacity, 1)) 0 25%," +   // --bg-opacity falls back on one in the instance that it does not load correctly
+            "rgba(255, 0, 246, var(--bg-opacity, 1)) 0 50%)", 
+        backgroundSize: "3rem 3rem",
+        animation: "2s bgFadeIn forwards",
+        backgroundPosition: "absolute"
+    };
+}
+
 export default function NotFound() {
     return (
-        <div style={{
-            // Draw missing-missing texture pattern in bg
-            backgroundImage: "repeating-conic-gradient(black 0 25%, #ff00f6 0 50%)",
-            backgroundSize: "3rem 3rem"
-        }} className="flex h-dvh items-center justify-center">
+        <div className="flex h-dvh items-center justify-center" data-is-not-found>
             <div className="flex h-min w-min flex-col items-center space-y-1 rounded-md bg-gray-800 p-2 shadow-[0_0_10rem_8rem_rgba(0,0,0,0.7)]">
                 <img src="https://api.mcheads.org/head/Omegadestroy400/256/hat" className="relative h-24 w-full rounded-t-xl" />  { /* This image is intentionally stretched. It helps fill the space, and it looks even more goofy */ }
                 <span className="flex items-center text-nowrap text-gray-200"> 
