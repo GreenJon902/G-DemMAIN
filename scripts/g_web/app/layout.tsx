5565e2682375d6d368e23adba06b88a2e6095d6f
GreenJon902 <greenjon902@gmail.com>
2026-04-20 14:26:10 +0100

    Fixed goddam linting errors

--- scripts/g_web/app/layout.tsx
+++ scripts/g_web/app/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({
 }) {
     return (
         <html>
-            <body className="p-4 bg-gray-900 text-white">
+            <body className="bg-gray-900 p-4 text-white">
                 {children}
             </body>
         </html>
