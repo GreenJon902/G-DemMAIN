8d3951d29dbfa483e146a0f0e69fa4b3412809e8
GreenJon902 <greenjon902@gmail.com>
2026-05-15 12:20:26 +0100

    Added proper nav bar

--- scripts/g_web/app/(main)/layout.tsx
+++ scripts/g_web/app/(main)/layout.tsx
@@ -1,13 +1,60 @@
 import "@/app/globals.css";
+import icon from "@/app/icon.png";
+import { hasSession } from "@/lib/auth";
+import { UserCircleIcon as UserCircleIconOutline } from "@heroicons/react/24/outline";
+import { UserCircleIcon as UserCircleIconSolid } from "@heroicons/react/24/solid";
+import Image from "next/image";
+import Link from "next/link";
 
-export default function Layout({
+export default async function Layout({
     children
 }: {
     children: React.ReactNode,
 }) {
     return (
         <html>
+            <head>
+                <link rel="icon" type="image/png" href={"icon.png"} />
+            </head>
             <body className="bg-gray-900 text-white">
+                <header><nav>
+                    <div className="flex flex-row bg-gray-700 p-1 flex-wrap">
+                        <Link href="/" className="size-6">
+                            <Image
+                                src={icon}
+                                className="size-full rounded-md hover:opacity-50"
+                                alt={"Logo"}
+                            />
+                        </Link>
+                        <Link href="/rules" className="ml-1 px-1 border-x border-gray-500 hover:bg-gray-600">
+                            Rules
+                        </Link>
+                        <Link href="/hisDoc" className="px-1 border-r border-gray-500 hover:bg-gray-600">
+                            HisDoc
+                        </Link>
+                        <Link href="/map" className="px-1 border-r border-gray-500 hover:bg-gray-600">
+                            Dynmap
+                        </Link>
+                        <Link href="/panel" className="px-1 border-r border-gray-500 hover:bg-gray-600">
+                            Panel
+                        </Link>
+                        <div className="flex-1">
+                            <div className="size-6 relative float-right text-white hover:text-gray-400">
+                                {  
+                                    (await hasSession()) ? (
+                                        <Link href="account" >
+                                            <UserCircleIconSolid className="size-full" />
+                                        </Link>
+                                    ) : (
+                                        <Link href="login?next=account" >
+                                            <UserCircleIconOutline className="size-full stroke-1" />
+                                        </Link>
+                                    )
+                                }
+                            </div>
+                        </div>
+                    </div>
+                </nav></header>
                 {children}
             </body>
         </html>
--- scripts/g_web/app/(main)/panel/layout.tsx
+++ scripts/g_web/app/(main)/panel/layout.tsx
@@ -11,10 +11,9 @@ export default function Layout({
 }) {
     return (
         <>
-            <header className="flex w-full flex-wrap items-center gap-2 bg-gray-700 p-2">
+            <header className="flex w-full flex-wrap items-center gap-2 bg-gray-700 p-2 border-t-gray-500 border-t">
                 {/* We give the text a very large flex so only it scales, however we still give the buttons flex so that they fill the entire width if they go on the newline */}
                 <div className="flex w-full flex-nowrap items-center gap-3 sm:flex-100">
-                    <Link href="/" className="h-min flex-1 rounded-full p-1 hover:bg-gray-800"><ArrowUturnLeftIcon className="size-6 stroke-2 text-gray-300" /></Link>  
                     <Link href="/panel" className="flex-100 text-3xl font-extrabold text-nowrap underline hover:text-gray-400">G-DemMAIN Panel</Link>
                 </div>
                 <div className="flex flex-1 flex-wrap items-center gap-2 sm:flex-nowrap">
