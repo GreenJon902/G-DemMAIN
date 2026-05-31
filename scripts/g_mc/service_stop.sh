689c7be724554a85779f6f3ccc799b09e73e4ad4
GreenJon902 <greenjon902@gmail.com>
2026-05-31 12:48:10 +0100

    Updated some environ

--- scripts/g_mc/service_stop.sh
+++ scripts/g_mc/service_stop.sh
@@ -2,15 +2,18 @@
 
 # Extract RCON settings from server.properties
 rcon_enabled=$(grep "^enable-rcon=" /var/lib/g_mc/server.properties | cut -d"=" -f2-)
-rcon_port=$(grep "^rcon.port=" /var/lib/g_mc/server.properties | cut -d"=" -f2-)
-rcon_password=$(grep "^rcon.password=" /var/lib/g_mc/server.properties | cut -d"=" -f2-)
+rcon_port=$RCON_PORT
+rcon_password=$RCON_PASSWORD
 
 # Check if RCON is enabled
 if [[ "$rcon_enabled" != "true" ]]; then
     echo "Warning, RCON is disabled, cannot stop the Minecraft server!"
     exit 1;
 elif [[ -z "$rcon_password" ]]; then  # Check if password length is equal to 0 (so was not set)
-    echo "Warning, RCON is disabled (as no password was set), cannot stop the Minecraft server!"
+    echo "Warning, could not find RCON_PASSWORD in environ!"
+    exit 1;
+elif [[ -z "$rcon_port" ]]; then  # Check if password length is equal to 0 (so was not set)
+    echo "Warning, could not find RCON_PORT in environ!"
     exit 1;
 fi
 
