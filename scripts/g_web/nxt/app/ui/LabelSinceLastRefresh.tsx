5c78ccdd6af075e40dee6effb4df5d78b02c4af6
GreenJon902 <greenjon902@gmail.com>
2026-06-22 22:15:25 +0100

    Changed wording

--- scripts/g_web/nxt/app/ui/LabelSinceLastRefresh.tsx
+++ scripts/g_web/nxt/app/ui/LabelSinceLastRefresh.tsx
@@ -29,7 +29,7 @@ export default function LabelSinceLastRefresh({
     
     return (
         <span className="text-gray-600">
-            Last updated {Math.floor((currentTimestamp - timestamp) / 1000)} seconds ago
+            Last synced {Math.floor((currentTimestamp - timestamp) / 1000)} seconds ago
         </span>
     );
 }
